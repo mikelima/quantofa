c06668d131e26bec626948b9a83e57701f1556c4
Luciano Montanaro <mikelima@cirulla.net>
2014-02-01 17:07:09 +0100

    Add italian translation

---
+++ i18n/it.ts
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it">
+<defaultcodec>UTF-8</defaultcodec>
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About Quanto Fa</source>
+        <translation>Informazioni su Quanto Fa</translation>
+    </message>
+    <message>
+        <source>Quanto Fa</source>
+        <translation></translation>
+    </message>
+    <message>
+        <source>version </source>
+        <translation>versione </translation>
+    </message>
+    <message>
+        <source>An RPN calculator based on the HP-35 simulator for from the &lt;a href=&apos;http://hpmuseum.org&apos;&gt;HP Museum&lt;/a&gt;.</source>
+        <translation>Una calcolatrice RPN basata sul simulatore di HP-35 dal sito &lt;a href=&apos;http://hpmuseum.org&apos;&gt;HP Museum&lt;/a&gt;. </translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;Copyright (c) 2014&lt;/p&gt;&lt;p&gt;Luciano Montanaro (&lt;a href=&apos;mailto:mikelima@cirulla.net&apos;&gt;mikelima@cirulla.net&lt;/a&gt;)&lt;/p&gt;</source>
+        <translation type="obsolete">&lt;p&gt;Copyright © 2014&lt;/p&gt;&lt;p&gt;Luciano Montanaro (&lt;a href=&apos;mailto:mikelima@cirulla.net&apos;&gt;mikelima@cirulla.net&lt;/a&gt;)&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;Copyright (c) 2004&lt;p&gt;&lt;a href=&apos;http://neil.fraser.name&apos;&gt;Neil Fraser&lt;/a&gt;, Elgin, Scotland.</source>
+        <translation type="obsolete">&lt;p&gt;Copyright © 2004&lt;p&gt;&lt;a href=&apos;http://neil.fraser.name&apos;&gt;Neil Fraser&lt;/a&gt;, Elgin, Scotland.</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;Licensed under the GNU Public License v2 or above&lt;/p&gt;</source>
+        <translation type="obsolete">&lt;p&gt;Rilasciato sotto licenza GNU Public License v2 o superiore&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>Copyright (c) 2014 Luciano Montanaro (&lt;a href=&apos;mailto:mikelima@cirulla.net&apos;&gt;mikelima@cirulla.net&lt;/a&gt;)</source>
+        <translation>Copyright © 2014 Luciano Montanaro (&lt;a href=&apos;mailto:mikelima@cirulla.net&apos;&gt;mikelima@cirulla.net&lt;/a&gt;)</translation>
+    </message>
+    <message>
+        <source>Copyright (c) 2004 &lt;a href=&apos;http://neil.fraser.name&apos;&gt;Neil Fraser&lt;/a&gt;, Elgin, Scotland.</source>
+        <translation>Copyright © 2004 &lt;a href=&apos;http://neil.fraser.name&apos;&gt;Neil Fraser&lt;/a&gt;, Elgin, Scotland.</translation>
+    </message>
+    <message>
+        <source>Licensed under the GNU Public License v2 or above</source>
+        <translation>Rilasciato sotto licenza GNU Public License v2 o superiore</translation>
+    </message>
+</context>
+<context>
+    <name>CalculatorPage</name>
+    <message>
+        <source>About Quanto Fa</source>
+        <translation>Informazioni su Quanto fa</translation>
+    </message>
+    <message>
+        <source>Quanto Fa</source>
+        <translation>Quanto Fa</translation>
+    </message>
+    <message>
+        <source>ENTER</source>
+        <translation>ENTER</translation>
+    </message>
+    <message>
+        <source>CH S</source>
+        <translation>CH S</translation>
+    </message>
+    <message>
+        <source>E EX</source>
+        <translation>E EX</translation>
+    </message>
+    <message>
+        <source>CL X</source>
+        <translation>CL X</translation>
+    </message>
+    <message>
+        <source>−</source>
+        <translation>−</translation>
+    </message>
+    <message>
+        <source>.</source>
+        <translation>,</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <source>Quanto Fa</source>
+        <translation>Quanto Fa</translation>
+    </message>
+</context>
+</TS>
